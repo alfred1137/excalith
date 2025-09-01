314e7d852d00c3feeb83563a4d43247c3d8333cb
Hiyun <alfred1137@gmail.com>
2025-09-01 09:13:54 +0800

    Squashed commit of the following:
    commit 22112c5652f229f0326a9539b95c14c25e4b3472
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Sun Feb 23 14:55:05 2025 +0300
    
        Bump version to v3.1.5
    
    commit 73eac266a6dfdb6fe023664cb8ff2ab319bd0a6a
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Sun Feb 23 13:35:37 2025 +0300
    
    commit f7c494bd0847379f090c483fe58f509631101922
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Sun Feb 23 12:48:38 2025 +0300
    
        Fix lint errors
    
    commit af24375b9bc0d46bbf126d9941bef1dfc101c138
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Sun Feb 23 12:38:14 2025 +0300
    
    commit 550831fd61ccfd40e7db650c318cfa35b0793cb8
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Sun Feb 23 12:05:45 2025 +0300
    
    commit b1e6b67b116fc8f20b5894b83e18bb466fb580a7
    Author: Rohit Manivel <66735279+rohit-iwnl@users.noreply.github.com>
    Date:   Sun Feb 23 01:41:30 2025 -0700
    
        Add gruvbox material theme variants (#84)
    
    commit 47a9f832cb784533b268b641045ebaf5006cf89e
    Author: Jannis Pohl <838818+jannispl@users.noreply.github.com>
    Date:   Sun Feb 23 09:15:58 2025 +0100
    
        Add PWA manifest and service worker (#89)
    
        * Add PWA manifest and service worker
        * Fix formatting
    
    commit ae32b8e65975b7bd2d8893356827b10a6c731fe7
    Author: Can Cellek <can.cellek@gmail.com>
    Date:   Tue Nov 12 10:07:08 2024 +0300
    
        Bump version to 3.1.4
    
    commit 40aedc9487dec3498db03b1ca776259b49191e02
    Author: Jannis Pohl <838818+jannispl@users.noreply.github.com>
    Date:   Tue Nov 12 07:57:14 2024 +0100
    
        Fix blurry text rendering on some platforms (#83)

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -11,7 +11,7 @@ const Meta = () => {
 
 	useEffect(() => {
 		// Set title
-		setTitle(settings.username + " Start Page")
+		setTitle(settings.title ? settings.title : settings.username + " Start Page")
 
 		// Return if there is no icon
 		if (!settings.fetch.image) return
@@ -39,6 +39,7 @@ const Meta = () => {
 			.catch((error) => {
 				console.error("Failed to fetch icon:", error)
 			})
+		// eslint-disable-next-line
 	}, [settings.fetch.image, settings.username])
 
 	return (
